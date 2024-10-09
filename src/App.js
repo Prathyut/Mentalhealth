76ac9261e2f206a5313d6841803f8ab4a77e5ffa
Prathyut <prathyushathota23@gmail.com>
2024-10-09 07:45:09 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import Appbar from './components/Appbar'; // Import the Appbar component
 import Home from './components/Home';       // Import Home component
 import AboutUs from './components/AboutUs'; // Import About Us component
-import Show from './components/Show';       // Import Show component
-import User from './components/User';       // Import User component
 import SignIn from './components/SignIn';   // Import SignIn component
 import SignUp from './components/SignUp';   // Import SignUp component
 import SignUpSuccess from './components/SignUpSuccess';
@@ -24,8 +22,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/show" element={<Show />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/signupsuccess" element={<SignUpSuccess />} />          
           <Route path="/signinsuccess" element={<SignInSuccess />} />        
